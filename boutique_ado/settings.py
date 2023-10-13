ebe62a52643e552aa91e50c5871349ec7e54d3b3
DeanBurke <deanburke07@hotmail.com>
2023-10-13 17:47:26 +0000

    Linked database, added procfile

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-m-m*!@i=npg%3i!kuv1jd@$#n8wiov&2jw=r*h(ij86n+8cj4j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-deanburke-boutiqueadov1-jdvz7xmsjuf.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = [ 'boutique-ado-db-49de5496647b.herokuapp.com', '8000-deanburke-boutiqueadov1-jdvz7xmsjuf.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -119,12 +120,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
